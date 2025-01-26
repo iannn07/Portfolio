c172b2d29e6435fa37f9b2ab1df0151303ab0db9
Iannn <pristianb007@gmail.com>
2025-01-26 22:56:22 +0800

    fix: Project List

--- src/components/ui/project-card.tsx
+++ src/components/ui/project-card.tsx
@@ -16,14 +16,14 @@ function ProjectCard({ issuer, title, image, roles, range }: ProjectCardProps) {
 
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ['start end', 'center start'],
+    offset: ['start end', 'start start'],
   })
 
   const opacity = useTransform(scrollYProgress, range, [0, 1])
 
   return (
     <motion.div
-      className="flex min-h-[40vh] w-full gap-10 rounded-2xl bg-background-secondary pl-5 pt-5 transition-transform duration-300 ease-in-out hover:rotate-[1deg] hover:shadow-xl md:pl-10 md:pt-10"
+      className="flex w-full flex-col rounded-xl bg-background-secondary p-5 transition-transform duration-300 ease-in-out hover:rotate-[1deg] hover:shadow-xl md:min-h-[40vh] md:flex-row md:gap-10 md:rounded-2xl md:p-0 md:pl-10 md:pt-10"
       style={{ opacity }}
       ref={container}
     >
@@ -31,11 +31,11 @@ function ProjectCard({ issuer, title, image, roles, range }: ProjectCardProps) {
         <h1 className="text-xl text-muted-foreground md:text-heading-6">
           {issuer}
         </h1>
-        <h1 className="h-full text-heading-3 font-medium md:text-heading-2">
+        <h1 className="h-full text-heading-4 font-medium md:text-heading-3">
           {title}
         </h1>
-        <div className="flex w-full flex-col gap-2">
-          <div className="flex gap-2">
+        <div className="mt-5 flex w-full flex-col gap-2">
+          <div className="flex flex-wrap gap-2">
             {roles.map((role, index) => (
               <Badge key={index} variant="secondary" className="w-fit">
                 {role}
@@ -45,11 +45,11 @@ function ProjectCard({ issuer, title, image, roles, range }: ProjectCardProps) {
         </div>
       </div>
       {image && (
-        <div className="flex w-3/4 items-end rounded-tl-3xl">
+        <div className="flex w-full justify-center rounded-br-2xl rounded-tl-3xl md:w-3/4 md:items-end md:justify-normal">
           <Image
             src={image}
             alt={title}
-            className="h-full w-full rounded-tl-3xl object-cover object-left"
+            className="h-full w-full rounded-lg object-cover object-left md:rounded-br-2xl md:rounded-tl-3xl"
             width={700}
             height={700}
           />
